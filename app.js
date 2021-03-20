500be6e5fda0f598665d79eb1df86879f9c5a381
DESKTOP-OGF00CJ\Amanda <gpfla7946@gmail.com>
2021-03-20 14:32:22 +0900

    210320 커밋 (#2.9까지)

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from "express"; // const express = require('express');
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter"; 
+import globalRouter from "./routers/globalRouter"; 
+/* default로 export 하지 않았으면 { userRouter } */
+
+const app = express();
+
+/* 위와 같은 코드 // 위의 코드를 arrow function 이라고 함
+function handleProfile(req, res) {
+    res.send("You are on my profile.");
+}
+*/
+
+app.use(cookieParser());
+app.use(express.urlencoded({extended:true})); // 서버가 form에서 받은 데이터를 이해할 수 있도록 
+app.use(express.json()); // 서버가 json 형식의 데이터를 이해할 수 있도록
+app.use(morgan("dev"));
+app.use(helmet());
+
+app.use("/", globalRouter);
+app.use("/user", userRouter); // 누군가 /user 경로로 들어오면 (그 하위 경로를 위해) 이 router 전체를 사용하겠다는 의미
+app.use("/video", videoRouter);
+
+export default app; // 누군가 내 파일을 불러올 때 app object를 주겠다.
+
+
+
+
+
+
+
