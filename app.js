9207a4d6b21e5a5369f0048fa3847ee853eefc11
DESKTOP-OGF00CJ\Amanda <gpfla7946@gmail.com>
2021-03-23 01:41:57 +0900

    2021.03.22 #2.14 까지

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter"; 
 import globalRouter from "./routers/globalRouter"; 
+import routes from "./routes";
 /* default로 export 하지 않았으면 { userRouter } */
 
 const app = express();
@@ -15,15 +16,18 @@ function handleProfile(req, res) {
 }
 */
 
+app.set('view engine', "pug");
+
+/* 미들웨어들 추가 */
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true})); // 서버가 form에서 받은 데이터를 이해할 수 있도록 
 app.use(express.json()); // 서버가 json 형식의 데이터를 이해할 수 있도록
 app.use(morgan("dev"));
 app.use(helmet());
 
-app.use("/", globalRouter);
-app.use("/user", userRouter); // 누군가 /user 경로로 들어오면 (그 하위 경로를 위해) 이 router 전체를 사용하겠다는 의미
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter); // 누군가 /user 경로로 들어오면 (그 하위 경로를 위해) 이 router 전체를 사용하겠다는 의미
+app.use(routes.videos, videoRouter);
 
 export default app; // 누군가 내 파일을 불러올 때 app object를 주겠다.
 
--- init.js
+++ init.js
@@ -1,5 +1,7 @@
 import app from "./app";
 
+/* init.js 에는 app.js 에서 import 한 application이 있음 */
+
 const PORT = 4000;
 
 
